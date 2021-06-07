365b5745f480a74ea79640dfdf27217d71a8db71
coderAurora <coderauroratm@gmail.com>
2021-06-06 23:05:15 -0400

    changed z index on both particles and input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -8,13 +8,25 @@ import 'tachyons';
 import Particles from './components/Particles/Particles';
 
 function App(props) {
+    const { input, setInput } = useState();
+
+    const onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const onButtonSubmit = () => {
+        console.log('click');
+    }
+
   return (
     <div className="App">
         <Particles />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+            onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}
+        />
            {/* <FaceRecognition />*/}
     </div>
   );
